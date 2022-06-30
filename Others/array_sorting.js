50be097124ed314369e9c5a40db778699354ff29
ShrikantJawla <shrikantjawla@gmail.com>
2022-06-30 21:34:12 +0530

    Added array sorting code

---
+++ Others/array_sorting.js
@@ -0,0 +1,15 @@
+let arr=[7,8,7,6,5,-5,4,-4,3,2,-10,-100,555,7777,888];
+let N=arr.length;
+for(let i=0; i<N; i++){
+    let min=i;
+    let j=i+1
+    for(; j<N; j++){
+        if(arr[j]<arr[min]) min=j;
+    }
+    if(i===j) continue;
+    let temp=arr[i];
+    arr[i]=arr[min];
+    arr[min]=temp;
+}
+
+console.log(arr);
